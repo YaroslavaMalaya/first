2a1c5268e190145dbbef450f171121e56548fefe
Katherine Shashkina <kshashkina@kse.org.ua>
2023-02-15 19:50:48 +0200

    added calculations + removed extra check in reverse polish notation

--- main/Program.cs
+++ main/Program.cs
@@ -2,7 +2,7 @@
 var variable = Console.ReadLine();
 var operators = new char[] { '+', '-', '/', '*' };
 List<string> operations = new List<string>();
-Stack<int> stack = new Stack<int>();
+Stack<double> stack = new Stack<double>();
 Queue<char?> queue = new Queue<char?>();
 
 var buff = "";
@@ -28,14 +28,6 @@ if (variable != null)
                 if (oper is '/' or '*')
                 {
                     operations.Add(oper.ToString());
-                    oper = ch;
-                    if (oper != '/' && oper != '*')
-                    {
-                        if (queue.Count > 0)
-                        {
-                            operations.Add(queue.Dequeue().ToString());
-                        }
-                    }
                 }
                 else
                 {
@@ -57,9 +49,43 @@ if (queue.Count == 1)
     operations.Add(queue.Dequeue().ToString());
 }
 
-foreach (var operation in operations)
-{
-    // Console.WriteLine(operation);
-}
 Console.WriteLine(string.Join(" ", operations));
-//      10-1+2*8-4 -> 10|1|-|2|8|*|+|4|-
+
+foreach (var element in operations)
+{
+    if (element != "+" && element != "-" && element != "/" && element != "*" && element != "^")
+    {
+        var db = Convert.ToDouble(element);
+        stack.Push(db);
+    }
+    else
+    {
+        var num1 = stack.Pop();
+        var num2 = stack.Pop();
+        if (element == "+")
+        {
+            var sum = num2 + num1;
+            stack.Push(sum);
+        }
+
+        if (element == "-")
+        {
+            var minus = num2 - num1;
+            stack.Push(minus);
+        }
+
+        if (element == "*")
+        {
+            var multiply = num2 * num1;
+            stack.Push(multiply);
+        }
+
+        if (element == "/")
+        {
+            var divide = num2 / num1;
+            stack.Push(divide);
+        }
+    }
+}
+
+Console.WriteLine(stack.Pop());
