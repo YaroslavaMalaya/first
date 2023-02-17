46a16db987129799a825760e76d4e5a3cd40f444
Katherine Shashkina <kshashkina@kse.org.ua>
2023-02-17 23:35:37 +0200

    changed the algorithm of converting into RPL, added support of brackets, fixed raising to a power (tests are still needed)

--- main/Program.cs
+++ main/Program.cs
@@ -1,11 +1,10 @@
 ﻿Console.WriteLine("Enter an example for calculation:");
 var variable = Console.ReadLine();
-var operators = new char[] { '+', '-', '/', '*', '^' };
+var operators = new char[] { '+', '-', '/', '*', '^', '(', ')' };
 List<string> operations = new List<string>();
-Stack<char?> stack = new Stack<char?>();
-Queue<double> queue = new Queue<double>();
-Stack<char?> stack1 = new Stack<char?>();
-
+Stack<double> stack = new Stack<double>();
+Stack<char?> stackOperators = new Stack<char?>();
+bool check = false;
 
 var buff = "";
 char? oper = null;
@@ -13,174 +12,123 @@ char? oper = null;
 if (variable != null)
     foreach (var ch in variable)
     {
-        if (stack1.Count == 0)
+        if (char.IsDigit(ch))
         {
-            if (char.IsDigit(ch))
-            {
-                buff += ch;
-                if ((stack.Contains('-') || stack.Contains('+')) && (oper is '+' or '-'))
-                {
-                    operations.Add(stack.Pop().ToString());
-                }
-            }
-            else if (operators.Contains(ch))
-            {
-                operations.Add(buff);
-                buff = "";
-                if (oper is not null)
-                {
-                    if (oper is '/' or '*' or '^')
-                    {
-                        if (ch != '(')
-                        {
-                            operations.Add(oper.ToString());
-                        }
-                        else
-                        {
-                            stack.Push(oper);
-                        }
-                    }
-                    else
-                    {
-                        stack.Push(oper);
-                    }
-                } 
-                oper = ch;
-            }
-            else if (ch == '(')
-            {
-                stack.Push(oper);
-                stack1.Push(ch);
-                oper = ch;
-            }
+            buff += ch;
         }
-        else
+        else if (operators.Contains(ch))
         {
-            if (char.IsDigit(ch))
+            operations.Add(buff);
+            buff = "";
+            if (ch is '^')
             {
-                buff += ch;
-                if ((stack1.Contains('-') || stack1.Contains('+')) && (oper is '+' or '-'))
+                stackOperators.Push(ch);
+            }
+            else if ((oper is '*' or '/' or '^') && stackOperators.Count > 0 && (ch != '(') && (check == false))
+            {
+                operations.Add(stackOperators.Pop().ToString());
+                if ((stackOperators.Contains('+') || stackOperators.Contains('-')) && (ch is '+' or '-'))
                 {
-                    operations.Add(stack1.Pop().ToString());
-                }
-            }
-            else if (operators.Contains(ch))
-            {
-                operations.Add(buff);
-                buff = "";
-                if (oper is not null && oper is not '(')
-                { 
-                    if (oper is '/' or '*' or '^')
-                    {
-                        if (ch != '(')
-                        {
-                            operations.Add(oper.ToString());
-                        }
-                        else
-                        {
-                            stack1.Push(oper);
-                        }
-                    }
-                    else
-                    {
-                        stack1.Push(oper);
-                    }
-                } 
-                oper = ch;
-            }
-            else
-            {
-                stack1.Push(oper);
-                oper = ch;
-            }
-            if (oper == ')')
-            {
-                operations.Add(buff);
-                buff = "";
-                if (ch != '/' && ch != '*')
-                {
-                    while (stack1.Count != 0)
-                    {
-                        operations.Add(stack1.Pop().ToString());
-                    }
-                    while (stack.Count != 0)
-                    {
-                        operations.Add(stack.Pop().ToString());
-                    }
-                    oper = ch;
+                    operations.Add(stackOperators.Pop().ToString());
+                    stackOperators.Push(ch); 
                 }
                 else
                 {
-                    oper = ch;
-                    stack1.Push(oper);
+                    stackOperators.Push(ch);
                 }
             }
+            
+            else if ((stackOperators.Contains('+') || stackOperators.Contains('-')) && (ch is '+' or '-') && (check == false))
+            {
+                operations.Add(stackOperators.Pop().ToString());
+                stackOperators.Push(ch);
+            }
+            else if (ch == '(')
+            {
+                check = true;
+            }
+            else if ((ch == ')')  && stackOperators.Count > 0)
+            {
+                check = false;
+                while (stackOperators.Count > 0)
+                {
+                    operations.Add(stackOperators.Pop().ToString());
+                }
+            }
+            else if (ch is not '(' or ')')
+            {
+                stackOperators.Push(ch);
+            }
+            oper = ch;
         }
     }
 
 if (buff != "")
 {
     operations.Add(buff);
-    operations.Add(oper.ToString());
+    if (stackOperators.Count == 0)
+    {
+        operations.Add(oper.ToString());
+    }
 }
 
-if (stack.Count == 1)
+while (stackOperators.Count > 0)
 {
-    operations.Add(stack.Pop().ToString());
+    operations.Add(stackOperators.Pop().ToString());
 }
 
-if (operations.Contains("(") || operations.Contains(")") )
+string itemToRemove = "";
+while (operations.Contains(""))
 {
-    operations.Remove("(");
-    operations.Remove(")");
+    operations.Remove(itemToRemove);
 }
 
+Console.WriteLine("RPL:");
 Console.WriteLine(string.Join(" ", operations));
+
+
 
 foreach (var element in operations)
 {
-    if (element == "")
-    {
-        continue;
-    }
     if (element != "+" && element != "-" && element != "/" && element != "*" && element != "^")
     {
         var db = Convert.ToDouble(element);
-        queue.Enqueue(db);
+        stack.Push(db);
     }
     else
     {
-        var num1 = queue.Dequeue();
-        var num2 = queue.Dequeue();
+        var num1 = stack.Pop();
+        var num2 = stack.Pop();
         if (element == "+")
         {
             var sum = num2 + num1;
-            queue.Enqueue(sum);
+            stack.Push(sum);
         }
 
         if (element == "-")
-        { 
+        {
             var minus = num2 - num1;
-            queue.Enqueue(minus);
+            stack.Push(minus);
         }
 
         if (element == "*")
         {
             var multiply = num2 * num1;
-            queue.Enqueue(multiply);
+            stack.Push(multiply);
         }
 
         if (element == "/")
         {
             var divide = num2 / num1;
-            queue.Enqueue(divide);
+            stack.Push(divide);
         }
         if (element == "^")
         {
             var expo = Math.Pow(num2, num1);
-            queue.Enqueue(expo);
+            stack.Push(expo);
         }
     }
 }
 
-Console.WriteLine(queue.Dequeue());
+Console.WriteLine("RESULT:");
+Console.WriteLine(stack.Pop());
