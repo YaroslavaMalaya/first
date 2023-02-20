4b86b69b8f5041a345ac36a0bd86c56888708709
Katherine Shashkina <kshashkina@kse.org.ua>
2023-02-20 20:13:18 +0200

    added Enqueue() and Clear() methods to Queue class( foreach method does not work )

--- main/queue.cs
+++ main/queue.cs
@@ -2,5 +2,22 @@ namespace main;
 
 public class Queue<T>
 {
-    
+    private const int Capacity = 50;
+    private T[] _array = new T[Capacity];
+    private int _pointer;
+
+    public void Enqueue(T value)
+    {
+        if (_pointer == _array.Length)
+        {
+            throw new Exception("Stack overflowed");
+        }
+
+        _array[_pointer++] = value;
+    }
+
+    public void Clear()
+    {
+        Array.Clear(_array);
+    }
 }
